8f02888b57ad1e171dac79cf7d60c0b54719bc39
ShaunZh <308826842@qq.com>
2024-03-12 18:53:39 +0800

    install mikro-orm & postgresql

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('hello world ------12121')
+import { MikroORM, Options } from '@mikro-orm/core'
+import { PostgreSqlDriver } from '@mikro-orm/postgresql';
+import { __prod__ } from './constants';
+
+
+const config: Options  = {
+    entities: [],
+    dbName: 'lireddit_server',
+    driver: PostgreSqlDriver,
+    debug: !__prod__,
+}
+
+const main = async () => {
+    const orm = await MikroORM.init(config)
+}
+
+main();
